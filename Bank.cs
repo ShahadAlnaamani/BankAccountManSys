4fe0059372662584f6a4c4b9be5c00417e599ea0
Shahad <Shahadalnaamani@outlook.com>
2024-09-25 15:18:12 +0400

    Created two classes and added skeleton code :)

---
+++ Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManSys
+{
+    internal class Bank
+    {
+        public List<int> BankAccounts { get; set; }
+
+
+        public void AddAcount()
+        { }
+
+        public void GetAccountByNumber() 
+        { }
+
+        public void DisplayAllAccounts() 
+        { }
+    }
+}
---
+++ BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManSys
+{
+    internal class BankAccount
+    {
+        private string AccountNumber;
+        private string AccountHolderName;
+        private decimal Balance;
+
+        public void AccountInfo(string accountHolderNumber, string accountNumber)
+        {
+            Balance = 0; //Setting defualt account balance 
+        }
+
+        public void InitialDeposit(double Deposit, string accountHolderNumber, string accountNumber)
+        {
+            
+        }
+
+
+        public void Deposit()
+        { 
+        
+        }
+
+        public void Withdraw()
+        {
+        
+        }
+
+        public void GetAccountInfo()
+        {
+        
+        }
+    }
+}
